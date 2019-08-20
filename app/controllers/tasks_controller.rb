e2f1373388b6988c93480a51d7ee7eaf92d6c55f
mayong <hhmayong@gmail.com>
2019-08-20 16:18:45 +0800

    completed challenge

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,45 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :delete]
   def index
     @tasks = Task.all
   end
 
   def show
-    @tasks = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    index
+    render :index
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    index
+    render :show
+  end
+
+  def delete
+    @task.destroy
+    index
+    render :index
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
